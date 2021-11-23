8d8d71d9b086e47e6411561476f7a9c70568b8e4
Tzach Baksis <tzachbaksis98@gmail.com>
2021-11-23 21:53:26 +0200

    utils: Added utils package with sockets useful APIs

---
+++ utils/sockets.py
@@ -0,0 +1,21 @@
+import socket
+
+BUFFER_SIZE = 2 ** 10
+
+
+def create_icmp_socket():
+    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    return icmp_socket
+
+
+def create_icmp_server_socket():
+    icmp_server_socket = create_icmp_socket()
+    icmp_server_socket.bind(("0.0.0.0", 0))
+    icmp_server_socket.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+    return icmp_server_socket
+
+
+def create_tcp_socket():
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    return tcp_socket
